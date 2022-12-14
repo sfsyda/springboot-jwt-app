0e119efd4d43da262d4690ae1b6214292445438b
sfsyda <shazzlles.fab@gmail.com>
2022-12-14 07:42:40 +0530

    Initial commit

---
+++ src/test/java/com/shaziya/springbootjwtatt/SpringbootJwtAttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaziya.springbootjwtatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtAttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
